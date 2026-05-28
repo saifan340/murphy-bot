509197e018cf97c89fbab3c5106b8ead4b6ef98b
Saifan Aremenak <saifanaremenak@gmail.com>
2026-05-28 20:52:12 +0200

    fix: update groq model name

--- api/chat.js
+++ api/chat.js
@@ -8,7 +8,7 @@ module.exports = async function handler(req, res) {
         "Authorization": `Bearer ${process.env.GROQ_API_KEY}`,
       },
       body: JSON.stringify({
-        model: "llama3-8b-8192",
+        model:"llama-3.1-8b-instant",
         messages: [
           { role: "system", content: req.body.system },
           ...req.body.messages
